7d1ff060f4b0c55ac9d1df05a0e24e4255ca53e1
hash0i <akhtar.hasm@gmail.com>
2026-01-09 19:16:20 +0500

    4th push

--- app/main.py
+++ app/main.py
@@ -3,16 +3,15 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    valid_commands = {'help'}
-
-    sys.stdout.write("$ ")
-    
-    command = input()
-    if command is not None and command not in valid_commands:
-        print(f"{command}: command not found")
         
-    elif command == "exit":
-        exit
+        command = exit
+        while(command):
+            sys.stdout.write("$ ")
+            command = input()
+            if command == "exit":
+                sys.exit(0)
+            else:
+                print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
